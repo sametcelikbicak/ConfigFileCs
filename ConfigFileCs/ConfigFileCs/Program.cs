5fac1f43e5e41889c84716f5fefd24116cacf2cd
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2016-05-19 13:47:51 +0300

    First commit.

---
+++ ConfigFileCs/ConfigFileCs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConfigFileCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Read.FromConfig("HelloWorld"));
+            Console.WriteLine(Read.FromConfig("KeyOne"));
+            Console.WriteLine(Read.FromConfig("KeyTwo"));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConfigFileCs/ConfigFileCs/Read.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConfigFileCs
+{
+    static public class Read
+    {
+        static public string FromConfig(String key)
+        {
+            return ConfigurationManager.AppSettings.Get(key);
+        }
+    }
+}
